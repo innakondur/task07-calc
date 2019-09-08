b5d9a11826dcf348d2b674b1eda16bdd4c51e8d2
innakondur <innakondur@gmail.com>
2019-09-08 12:03:45 +0000

    start

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const nums = document.querySelectorAll('.calc .nums button');
+const opers = document.querySelectorAll('.calc .opers button');
+const display = document.querySelector('.calc .display');
+const equal = document.querySelector('.calc .eq');
+nums.forEach( buttonNum => buttonNum.addEventListener('click', numPressed) );
+    function numPressed(ev) {
+        const num = ev.target.innerText;
+        display.value += num;
+    }
+opers.forEach( buttonOper => buttonOper.addEventListener('click', operPressed) );
+    function operPressed(ev) {
+        const oper = ev.target.innerText;
+        display.value += oper;
+    }
+equal.addEventListener('click', () => display.value = eval(display.value) );
